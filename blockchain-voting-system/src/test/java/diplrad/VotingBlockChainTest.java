08e9a06a1be6d973249a30fae59b0c163e754328
Joana311 <deptop725+3@gmail.com>
2024-03-02 17:36:34 +0100

    Implement voting blockchain with tests and a main class

---
+++ blockchain-voting-system/src/test/java/diplrad/VotingBlockChainTest.java
@@ -0,0 +1,54 @@
+package diplrad;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class VotingBlockChainTest {
+
+    public static VotingBlockChain setUpBlockchain()  {
+        List<String> candidates = List.of("Candidate1", "Candidate2", "Candidate3");
+        return new VotingBlockChain(candidates);
+    }
+
+    @Test
+    public void givenBlockchain_whenNotChanged_thenValidationOk() {
+        BlockChain blockchain = setUpBlockchain();
+        Block firstBlock = new Block("Candidate1", blockchain.getLastBlockHash());
+        blockchain.mineBlock(firstBlock);
+        Block secondBlock = new Block("Candidate2", blockchain.getLastBlock().getHash());
+        blockchain.mineBlock(secondBlock);
+        Block thirdBlock = new Block("Candidate3", blockchain.getLastBlock().getHash());
+        blockchain.mineBlock(thirdBlock);
+        boolean validationResult = blockchain.validate();
+        assertTrue(validationResult);
+    }
+
+    @Test
+    public void givenBlockchain_whenChanged_thenValidationFailed() {
+        BlockChain blockchain = setUpBlockchain();
+        Block firstBlock = new Block("Candidate1", blockchain.getLastBlockHash());
+        blockchain.mineBlock(firstBlock);
+        Block secondBlock = new Block("Candidate2", blockchain.getLastBlock().getHash());
+        blockchain.mineBlock(secondBlock);
+        Block thirdBlock = new Block("Candidate3", blockchain.getLastBlock().getHash());
+        blockchain.mineBlock(thirdBlock);
+        firstBlock.setData("Candidate3");
+        secondBlock.setData("Candidate3");
+        boolean validationResult = blockchain.validate();
+        assertFalse(validationResult);
+    }
+
+    @Test
+    public void givenBlockchain_whenDataNotCandidate_thenValidationFailed() {
+        BlockChain blockchain = setUpBlockchain();
+        Block firstBlock = new Block("Candidate4", blockchain.getLastBlockHash());
+        blockchain.mineBlock(firstBlock);
+        boolean validationResult = blockchain.validate();
+        assertFalse(validationResult);
+    }
+
+}
