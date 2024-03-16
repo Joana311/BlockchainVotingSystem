d50c94a9d2c7b3d48486272fbba8209cbdac72ba
Joana311 <deptop725+3@gmail.com>
2024-03-16 23:32:58 +0100

    Connect database and create first migration

---
+++ CentralPeerCoordinator/CentralPeerCoordinator.Data.Db/Migrations/20240316222354_CreatePeerTable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CentralPeerCoordinator.Data.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatePeerTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Professionals",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IpAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Port = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Professionals", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Professionals");
+        }
+    }
+}
