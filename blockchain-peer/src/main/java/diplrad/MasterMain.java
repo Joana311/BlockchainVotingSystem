e08cb92cbf7666dfa5d1aab97ce424b34920243f
Joana311 <deptop725+3@gmail.com>
2024-03-24 13:57:46 +0100

    Remove unnecessary code from MasterMain

--- blockchain-peer/src/main/java/diplrad/MasterMain.java
+++ blockchain-peer/src/main/java/diplrad/MasterMain.java
@@ -5,11 +5,7 @@ import diplrad.http.HttpSender;
 import diplrad.models.Peer;
 import diplrad.models.PeerRequest;
 import diplrad.models.VotingBlockChain;
-import diplrad.tcp.BlockchainTcpClient;
 import diplrad.tcp.TcpServer;
-
-import java.io.IOException;
-import java.util.List;
 
 import static diplrad.helpers.FileReader.readCandidatesFromFile;
 import static diplrad.helpers.IpHelper.getOwnIpAddress;
@@ -18,30 +14,14 @@ public class MasterMain {
 
     public static void main(String[] args) {
 
+        VotingBlockChain.createInstance(readCandidatesFromFile());
+
         TcpServer.TcpServerThread t = new TcpServer.TcpServerThread();
         t.start();
 
-        PeerRequest peerRequest = new PeerRequest(getOwnIpAddress(), Constants.TCP_SERVER_PORT);
-
         HttpSender httpSender = new HttpSender();
-        Peer ownPeer = httpSender.registerPeer(peerRequest);
-        List<Peer> peers = httpSender.getPeers();
-
-        VotingBlockChain.createInstance(readCandidatesFromFile());
-
-        for (Peer peer : peers) {
-
-            try {
-                BlockchainTcpClient client = new BlockchainTcpClient();
-                client.startConnection(peer.getIpAddress().getHostAddress(), peer.getPort());
-                client.sendBlockchain(VotingBlockChain.getInstance());
-                client.stopConnection();
-            } catch (IOException e) {
-                System.out.println("Unable to start TCP client.");
-                System.exit(1);
-            }
-
-        }
+        PeerRequest ownPeerRequest = new PeerRequest(getOwnIpAddress(), Constants.TCP_SERVER_PORT);
+        Peer ownPeer = httpSender.registerPeer(ownPeerRequest);
 
     }
 
