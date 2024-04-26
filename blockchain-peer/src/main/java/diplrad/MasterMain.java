c7c6888070bc90f989b9ff5e03cdafc5c4098491
Joana311 <deptop725+3@gmail.com>
2024-04-26 16:19:38 +0200

    Implement Azure queue message handler

--- blockchain-peer/src/main/java/diplrad/MasterMain.java
+++ blockchain-peer/src/main/java/diplrad/MasterMain.java
@@ -1,21 +1,13 @@
 package diplrad;
 
-import com.azure.identity.DefaultAzureCredentialBuilder;
-import com.azure.storage.queue.QueueClient;
-import com.azure.storage.queue.QueueClientBuilder;
-import com.azure.storage.queue.QueueServiceClient;
-import com.azure.storage.queue.QueueServiceClientBuilder;
-import com.azure.storage.queue.models.QueueServiceProperties;
-import com.azure.storage.queue.models.SendMessageResult;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import diplrad.constants.LogMessages;
 import diplrad.exceptions.*;
-import diplrad.helpers.BlockChainTcpClientHelper;
 import diplrad.helpers.PeerHttpHelper;
-import diplrad.helpers.VoteMocker;
 import diplrad.http.HttpSender;
 import diplrad.models.blockchain.VotingBlockChainSingleton;
+import diplrad.queue.StorageQueueClient;
 import diplrad.tcp.TcpServer;
 
 import static diplrad.helpers.ExceptionHandler.handleFatalException;
@@ -46,46 +38,10 @@ public class MasterMain {
             handleFatalException(e);
         }
 
-        // this is vote mocker part, used only for testing purposes
-
-        /*try {
-            for (int i = 0; i < 10; i++) {
-                Thread.sleep((long)(Math.random() * 20000));
-                VoteMocker.generateRandomVotes(VotingBlockChainSingleton.getInstance());
-                BlockChainTcpClientHelper.createTcpClientsAndSendBlockChains(gson);
-            }
-        } catch (TcpException e) {
-            handleFatalException(e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }*/
-
-        String queueName = "myqueue";
-
-        QueueClient queueClient = new QueueClientBuilder()
-                .endpoint("https://127.0.0.1:10001/devstoreaccount1/")
-                .queueName(queueName)
-                .credential(new DefaultAzureCredentialBuilder().build())
-                .buildClient();
-
-        queueClient.sendMessage("First message");
-        queueClient.sendMessage("Second message");
-
-        // Save the result so we can update this message later
-        SendMessageResult result = queueClient.sendMessage("Third message");
-
-
-        String queueServiceURL = "http://127.0.0.1:10001";
-        QueueServiceClient queueServiceClient = new QueueServiceClientBuilder().endpoint(queueServiceURL).buildClient();
-        QueueClient queueClient2 = queueServiceClient.createQueue("myQueue");
-        QueueServiceProperties properties = queueServiceClient.getProperties();
-        queueClient.sendMessage("myMessage");
-        queueClient.sendMessage("myMessage2");
-        queueClient.sendMessage("myMessage3");
-        queueClient.receiveMessages(10).forEach(message ->
-                System.out.println(message.getBody().toString()));
-
-        var a = 9;
+        StorageQueueClient storageQueueClient = new StorageQueueClient(gson);
+        while (true) {
+            storageQueueClient.receiveAndHandleQueueMessage();
+        }
 
     }
 
